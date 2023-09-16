309fb77baac28389839817833503ead593a33892
Landon Noss <landon.noss@sony.com>
2023-09-15 18:55:13 -0700

    uhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,20 @@
 	import Realm from '$lib/realm/Realm.svelte';
 	import { EditorState, type EditorRuntimeState } from '$lib/stores/index.js';
 	import { Canvas, T } from '@threlte/core';
-	import { HTML } from '@threlte/extras';
+	import { HTML, OrbitControls } from '@threlte/extras';
 	import { World } from '@threlte/rapier';
-	import Mousetrap from 'mousetrap';
+	import Mousetrap, { type ExtendedKeyboardEvent } from 'mousetrap';
 	import { onMount } from 'svelte';
 	import { spring } from 'svelte/motion';
-	import { type PerspectiveCamera } from 'three';
+	import { MOUSE, type PerspectiveCamera } from 'three';
 
 	let EditorElement: HTMLElement;
 	onMount(async () => {
 		const { createEditor } = await import('../lib/editor');
 		createEditor(EditorElement);
 
-		Mousetrap.bind('~', (e: KeyboardEvent) => {
+		Mousetrap.bind('i d d q d', (e: ExtendedKeyboardEvent) => {
+			console.log('god mode activated');
 			EditorState.update((c: EditorRuntimeState) => {
 				c.visible = !c.visible;
 				return c;
@@ -66,11 +67,11 @@
 
 <style>
 	.rete {
-		position: relative;
+		position: absolute;
 		width: 100vw;
-		height: 100vh;
+		height: 80vh;
+		margin: 10vw auto;
 		font-size: 1rem;
 		text-align: left;
-		border: 1px solid #cbd0db;
 	}
 </style>

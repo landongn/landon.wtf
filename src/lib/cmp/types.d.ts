222de6f2914f14163dc1248c93ce5b0e83f583d7
Landon Noss <landongn@gmail.com>
2023-09-16 10:56:21 -0700

    adding some types and enums for objects and animations

---
+++ src/lib/cmp/types.d.ts
@@ -0,0 +1,66 @@
+interface EasingTypes {
+	backInOut(t: number): number;
+	backIn(t: number): number;
+	backOutbackIn(t: number): number;
+	bounceOutbackIn(t: number): number;
+	bounceInOutbackIn(t: number): number;
+	bounceInbackIn(t: number): number;
+	circInOutbackIn(t: number): number;
+	circInbackIn(t: number): number;
+	circOutbackIn(t: number): number;
+	cubicInOutbackIn(t: number): number;
+	cubicInbackIn(t: number): number;
+	cubicOutbackIn(t: number): number;
+	elasticInOutbackIn(t: number): number;
+	elasticInbackIn(t: number): number;
+	elasticOutbackIn(t: number): number;
+	expoInOutbackIn(t: number): number;
+	expoInbackIn(t: number): number;
+	expoOutbackIn(t: number): number;
+	quadInOutbackIn(t: number): number;
+	quadInbackIn(t: number): number;
+	quadOutbackIn(t: number): number;
+	quartInOutbackIn(t: number): number;
+	quartInbackIn(t: number): number;
+	quartOutbackIn(t: number): number;
+	quintInOutbackIn(t: number): number;
+	quintInbackIn(t: number): number;
+	quintOutbackIn(t: number): number;
+	sineInOutbackIn(t: number): number;
+	sineInbackIn(t: number): number;
+	sineOutbackIn(t: number): number;
+	linear(x: number): number;
+}
+enum Tweens {
+	backInOut,
+	backIn,
+	backOutbackIn,
+	bounceOutbackIn,
+	bounceInOutbackIn,
+	bounceInbackIn,
+	circInOutbackIn,
+	circInbackIn,
+	circOutbackIn,
+	cubicInOutbackIn,
+	cubicInbackIn,
+	cubicOutbackIn,
+	elasticInOutbackIn,
+	elasticInbackIn,
+	elasticOutbackIn,
+	expoInOutbackIn,
+	expoInbackIn,
+	expoOutbackIn,
+	quadInOutbackIn,
+	quadInbackIn,
+	quadOutbackIn,
+	quartInOutbackIn,
+	quartInbackIn,
+	quartOutbackIn,
+	quintInOutbackIn,
+	quintInbackIn,
+	quintOutbackIn,
+	sineInOutbackIn,
+	sineInbackIn,
+	sineOutbackIn,
+	linear
+}
